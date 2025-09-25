5f0cd3f2fe1da015afb7f2976a3a74f7c7f6c9d1
skharichandan <140094399+skharichandan@users.noreply.github.com>
2025-09-25 15:10:40 +0530

    Create customer_curated.sql

---
+++ stedi_human_balance_analytics/CURATED ZONE/SQL/customer_curated.sql
@@ -0,0 +1,23 @@
+CREATE EXTERNAL TABLE `customer_curated`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://final-project-stedi-human-balance-analytics/customer/curated/'
+TBLPROPERTIES (
+  'CreatedByJob'='Customer Trusted Zone to Customer Curated Zone', 
+  'CreatedByJobRun'='jr_08d2b2c032209511040aefe6edd23cd8dc22891aab68e9fc8ba277d0fafb7d75', 
+  'classification'='json')
