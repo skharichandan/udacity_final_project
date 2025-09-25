ff561fce77a089d9d6ebb468a52672bc56a30075
skharichandan <140094399+skharichandan@users.noreply.github.com>
2025-09-25 15:02:22 +0530

    Create customer_trusted.sql

---
+++ stedi_human_balance_analytics/TRUSTED ZONE/SQL/customer_trusted.sql
@@ -0,0 +1,23 @@
+CREATE EXTERNAL TABLE `customer _trusted`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://final-project-stedi-human-balance-analytics/customer/trusted/'
+TBLPROPERTIES (
+  'CreatedByJob'='Customer Landing Zone to Customer Trusted Zone', 
+  'CreatedByJobRun'='jr_672787981906bedc868f9d87635dc2f61f5f2148b0f98549cf45da541d4da7ff', 
+  'classification'='json')
